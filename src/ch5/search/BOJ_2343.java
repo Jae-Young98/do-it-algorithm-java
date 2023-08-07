b56a5976dcad6cc8ef652eda0beafd158f7ab500
Jae-Young98 <ygy0102@gmail.com>
2023-08-07 20:17:21 +0900

    블루레이 만들기

---
+++ src/ch5/search/BOJ_2343.java
@@ -0,0 +1,49 @@
+package ch5.search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2343 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int start = 0;
+        int end = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (start < arr[i]) {
+                start = arr[i];
+            }
+            end += arr[i];
+        }
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int sum = 0;
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                if (sum + arr[i] > mid) {
+                    cnt++;
+                    sum = 0;
+                }
+                sum += arr[i];
+            }
+            if (sum != 0) {
+                cnt++;
+            }
+            if (cnt > m) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        System.out.println(start);
+    }
+}
