d028cef03cf9d3f43274d45b2c8837d7c6c5e594
Jae-Young98 <ygy0102@gmail.com>
2023-08-15 19:25:27 +0900

    거의 소수

---
+++ src/ch7/numberTheory/BOJ_1456.java
@@ -0,0 +1,42 @@
+package ch7.numberTheory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1456 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long[] arr = new long[10000001];
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 2; i <= Math.sqrt(arr.length); i++) {
+            if (arr[i] == 0) {
+                continue;
+            }
+            for (int j = i + i; j < arr.length; j = j + i) {
+                arr[j] = 0;
+            }
+        }
+
+        int result = 0;
+        for (int i = 2; i < 10000001; i++) {
+            if (arr[i] != 0) {
+                long tmp = arr[i];
+                while ((double)arr[i] <= (double)b/(double)tmp) {
+                    if ((double)arr[i] >= (double)a/(double)tmp) {
+                        result++;
+                    }
+                    tmp *= arr[i];
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
