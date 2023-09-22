ccb2bef83797039aac68b5812657b1452720657c
Jae-Young98 <ygy0102@gmail.com>
2023-09-23 01:33:45 +0900

    경로찾기

---
+++ src/ch8/graph/BOJ_11403.java
@@ -0,0 +1,41 @@
+package ch8.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11403 {
+
+    static int n;
+    static int[][] distance;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        distance = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int v = Integer.parseInt(st.nextToken());
+                distance[i][j] = v;
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (distance[i][k] == 1 && distance[k][j] == 1) {
+                        distance[i][j] = 1;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(distance[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
